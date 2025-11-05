fbb10788c54206f5aabb997e0e685b7e331bb880
minseo_song <phmsoft30@gmail.com>
2025-11-05 10:17:31 +0900

    [Merge] 조작 테스트 용 Input Manager 등

---
+++ Naknak/Assets/Scripts/LastInputManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+/// <summary>
+/// 가로와 세로 입력의 마지막 입력을 처리하는 클래스
+/// GetAxisRaw("Horizontal") or GetAxizRaw("Vertical")
+/// </summary>
+public class LastInputManager : MonoBehaviour
+{
+    private float lastHorizontal = 0f;
+    private float lastVertical = 0f;
+
+    void Update()
+    {
+        // 가로 입력 처리
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) lastHorizontal = -1f;
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) lastHorizontal = 1f;
+
+        // Key Up
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A))
+        {
+            // 만약 반대 키가 눌려있다면, 그 방향으로 전환
+            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) lastHorizontal = 1f;
+            else lastHorizontal = 0f;
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) lastHorizontal = -1f;
+            else lastHorizontal = 0f;
+        }
+
+        // 세로 입력 처리
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) lastVertical = -1f;
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) lastVertical = 1f;
+
+        // Key Up
+        if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) lastVertical = 1f;
+            else lastVertical = 0f;
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) lastVertical = -1f;
+            else lastVertical = 0f;
+        }
+    }
+
+    public float GetAxisRaw(string axisName)
+    {
+        if (axisName == "Horizontal")
+        {
+            return lastHorizontal;
+        }
+        else if (axisName == "Vertical")
+        {
+            return lastVertical;
+        }
+        return 0f;
+    }
+}
