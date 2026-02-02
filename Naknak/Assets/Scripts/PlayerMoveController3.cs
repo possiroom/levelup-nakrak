3c131e7d01203308eda2c2e40acd77e6fed21e10
possiroom <178400765+possiroom@users.noreply.github.com>
2026-02-02 22:00:44 +0900

    npcColider 수정본

--- Naknak/Assets/Scripts/PlayerMoveController3.cs
+++ Naknak/Assets/Scripts/PlayerMoveController3.cs
@@ -384,9 +384,9 @@ public class PlayerMoveController3 : MonoBehaviour
         Debug.DrawRay(from - new Vector3(0f, 0.5f, 0f), dir * moveDistance, Color.red);
 
         // Delayed Evaluation
-        check = MapManager.Instance.IsCollision(from - new Vector3(0f, 0.5f, 0f) + dir * moveDistance - gridPreset)
-            || (hit.collider != null && hit.collider.CompareTag("Interactable"));
-        //check = hit.collider != null && !hit.collider.CompareTag("Interactable");
+        //check = MapManager.Instance.IsCollision(from - new Vector3(0f, 0.5f, 0f) + dir * moveDistance - gridPreset)
+        //    || (hit.collider != null && hit.collider.CompareTag("Interactable"));
+        check = hit.collider != null;
 
         return check;
     }
